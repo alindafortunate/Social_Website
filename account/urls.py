89e59b0cb94fc9f1c9f4de55c9c47c38dff2126a
alindafortunate <alindafortunate5@gmail.com>
2025-05-31 11:47:53 +0300

    Implemented the class_based LogoutView though still missing a form since it accepts only POST requests.

--- account/urls.py
+++ account/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from django.contrib.auth.views import LoginView
+from django.contrib.auth import views as auth_views
 
 from .views import index
 
 urlpatterns = [
     path("", index, name="home"),
-    path("login/", LoginView.as_view(), name="login"),
+    path("login/", auth_views.LoginView.as_view(), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
 ]
