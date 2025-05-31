8ea4a89272f2ff997ec04665872092914a4a358a
alindafortunate <alindafortunate5@gmail.com>
2025-05-31 16:17:22 +0300

    Added the logout form to dashboard.html file

--- account/urls.py
+++ account/urls.py
@@ -4,9 +4,9 @@ from django.contrib.auth import views as auth_views
 from .views import index, test_view, dashboard
 
 urlpatterns = [
-    path("", index, name="home"),
+    path("home/", index, name="home"),
     path("test/", test_view, name="test"),
     path("login/", auth_views.LoginView.as_view(), name="login"),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
-    path("dashboard/", dashboard, name="dashboard"),
+    path("", dashboard, name="dashboard"),
 ]
