b5a468aede21d8a08b19a64cb0db94a5ab604e36
alindafortunate <alindafortunate5@gmail.com>
2025-06-02 14:58:52 +0300

    Implemented the Change Password Feature

--- account/urls.py
+++ account/urls.py
@@ -4,9 +4,19 @@ from django.contrib.auth import views as auth_views
 from .views import index, test_view, dashboard
 
 urlpatterns = [
+    path("", dashboard, name="dashboard"),
     path("home/", index, name="home"),
     path("test/", test_view, name="test"),
     path("login/", auth_views.LoginView.as_view(), name="login"),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
-    path("", dashboard, name="dashboard"),
+    path(
+        "password_change/",
+        auth_views.PasswordChangeView.as_view(),
+        name="password_change",
+    ),
+    path(
+        "password_change_done/",
+        auth_views.PasswordChangeDoneView.as_view(),
+        name="password_change_done",
+    ),
 ]
