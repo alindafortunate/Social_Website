ab7a24e979a139ce79292d82fdc57d24362119bc
alindafortunate <alindafortunate5@gmail.com>
2025-06-02 21:52:29 +0300

    Replaced the much long line of authentication in account/urls.py with django.contrib.auth.urls by the include function

--- account/urls.py
+++ account/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from django.contrib.auth import views as auth_views
 
 from .views import index, test_view, dashboard
@@ -7,34 +7,38 @@ urlpatterns = [
     path("", dashboard, name="dashboard"),
     path("home/", index, name="home"),
     path("test/", test_view, name="test"),
-    path("login/", auth_views.LoginView.as_view(), name="login"),
-    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
     path(
-        "password_change/",
-        auth_views.PasswordChangeView.as_view(),
-        name="password_change",
-    ),
-    path(
-        "password_change_done/",
-        auth_views.PasswordChangeDoneView.as_view(),
-        name="password_change_done",
-    ),
-    path(
-        "password_reset/", auth_views.PasswordResetView.as_view(), name="password_reset"
-    ),
-    path(
-        "password_reset_done/",
-        auth_views.PasswordResetDoneView.as_view(),
-        name="password_reset_done",
-    ),
-    path(
-        "password_reset_confirm/<uidb64>/<token>/",
-        auth_views.PasswordResetConfirmView.as_view(),
-        name="password_reset_confirm",
-    ),
-    path(
-        "password_reset_complete/",
-        auth_views.PasswordResetCompleteView.as_view(),
-        name="password_reset_complete",
-    ),
+        "", include("django.contrib.auth.urls")
+    ),  # This code represents the below lines about authentication
+    
+    # path("login/", auth_views.LoginView.as_view(), name="login"),
+    # path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    # path(
+    #     "password_change/",
+    #     auth_views.PasswordChangeView.as_view(),
+    #     name="password_change",
+    # ),
+    # path(
+    #     "password_change_done/",
+    #     auth_views.PasswordChangeDoneView.as_view(),
+    #     name="password_change_done",
+    # ),
+    # path(
+    #     "password_reset/", auth_views.PasswordResetView.as_view(), name="password_reset"
+    # ),
+    # path(
+    #     "password_reset_done/",
+    #     auth_views.PasswordResetDoneView.as_view(),
+    #     name="password_reset_done",
+    # ),
+    # path(
+    #     "password_reset_confirm/<uidb64>/<token>/",
+    #     auth_views.PasswordResetConfirmView.as_view(),
+    #     name="password_reset_confirm",
+    # ),
+    # path(
+    #     "password_reset_complete/",
+    #     auth_views.PasswordResetCompleteView.as_view(),
+    #     name="password_reset_complete",
+    # ),
 ]
