a9f4379e2245fcaf11b9caf410631f40fbe669fb
alindafortunate <alindafortunate5@gmail.com>
2025-06-03 13:14:02 +0300

    Implemented the register.html template and had forgotten to load the form variable with as_p and had used as_P which had caused issues.

--- account/views.py
+++ account/views.py
@@ -49,7 +49,7 @@ def register(request):
             new_user.set_password(user_form.cleaned_data["password"])
             # Then save it to the database.
             new_user.save()
-        return render(request, "account/register_done.html", {"new_user": new_user})
+            return render(request, "account/register_done.html", {"new_user": new_user})
     else:
         user_form = UserRegistrationForm()
-        return render(request, "account/register.html", {"user_form": user_form})
+    return render(request, "account/register.html", {"user_form": user_form})
