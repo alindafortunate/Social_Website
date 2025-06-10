93f3bf91d6ff6733867592d0d65471120fde85b6
alindafortunate <alindafortunate5@gmail.com>
2025-06-10 12:47:48 +0300

    Started on chapter 5, now understanding the messages framework for monitoring user actions.

--- account/views.py
+++ account/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
+from django.contrib import messages
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
@@ -68,6 +69,7 @@ def edit(request):
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
+            
     else:
         user_form = UserEditForm(instance=request.user)
         profile_form = ProfileEditForm(instance=request.user.profile)
