571b0d3f6fc094a23dcabd0e42c890419bbb23da
alindafortunate <alindafortunate5@gmail.com>
2025-06-10 14:20:59 +0300

    Defined the edit view and decorated it with a login_required decorator because for a user to be able to edit there profiles they should be logged.

--- account/views.py
+++ account/views.py
@@ -55,3 +55,8 @@ def register(request):
     else:
         user_form = UserRegistrationForm()
     return render(request, "account/register.html", {"user_form": user_form})
+
+
+@login_required
+def edit(request):
+    pass
