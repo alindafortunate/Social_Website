fe13e0181e8f3a8c490cc1630a7d16b5b3888576
alindafortunate <alindafortunate5@gmail.com>
2025-06-10 14:37:24 +0300

    Re-implemented the edit view and understood it.

--- account/views.py
+++ account/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-from .forms import LoginForm, UserRegistrationForm
+from django.contrib import messages
+from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
 from .models import Profile
 
 
@@ -59,4 +60,24 @@ def register(request):
 
 @login_required
 def edit(request):
-    pass
+    if request.method == "POST":
+        user_form = UserEditForm(instance=request.user, data=request.POST)
+        profile_form = ProfileEditForm(
+            instance=request.user.profile, data=request.POST, files=request.FILES
+        )
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, "User Profile Update was successful, thank you.")
+        else:
+            messages.error(
+                request, "Invalid, User Profile Update failed, cross check the fields."
+            )
+    else:
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(instance=request.user.profile)
+    return render(
+        request,
+        "account/edit.html",
+        {"user_form": user_form, "profile_form": profile_form},
+    )
