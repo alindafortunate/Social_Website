0ae0cf8ba6dd7fc40acb3fb4d5a5dcce0e9806ac
alindafortunate <alindafortunate5@gmail.com>
2025-06-10 13:00:12 +0300

    Changed the location of base.html file and added some messages to the edit view

--- account/views.py
+++ account/views.py
@@ -69,7 +69,10 @@ def edit(request):
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
-            
+            messages.success(request, "User profile update was successful.")
+        else:
+            messages.error(request, "Error during profile update.")
+
     else:
         user_form = UserEditForm(instance=request.user)
         profile_form = ProfileEditForm(instance=request.user.profile)
