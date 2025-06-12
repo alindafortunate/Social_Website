679ff758f1ae763f5fd423d3b4f47ac033fdec9e
alindafortunate <alindafortunate5@gmail.com>
2025-06-12 21:36:53 +0300

    Re-implemeted the EmailAuthBackend.

--- account/urls.py
+++ account/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path(
         "", include("django.contrib.auth.urls")
     ),  # This code represents the below lines about authentication
-    path("login/", auth_views.LoginView.as_view(), name="login"),
+    # path("login/", auth_views.LoginView.as_view(), name="login"),
     # path("logout/", auth_views.LogoutView.as_view(), name="logout"),
     # path(
     #     "password_change/",
