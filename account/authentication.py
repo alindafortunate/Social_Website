8f54fb47c23b9e3ba6aaef64bab3acb23c411cc8
alindafortunate <alindafortunate5@gmail.com>
2025-06-13 11:51:49 +0300

    The authenticate() method takes request parameter and two other optional parameters, i.e username and password.

--- account/authentication.py
+++ account/authentication.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User
 
 
 class EmailAuthBackend:
-    def authenticate(self):
+    def authenticate(self, request, username=None,password=None):
         pass
 
     def get_user(self):
