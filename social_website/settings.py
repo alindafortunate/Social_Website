c776062a6228daecd8aa2bd900ceab3a9f902791
alindafortunate <alindafortunate5@gmail.com>
2025-06-30 21:26:37 +0300

    Added a feature for creating profiles for users who login with Social Auth

--- social_website/settings.py
+++ social_website/settings.py
@@ -83,20 +83,20 @@ WSGI_APPLICATION = "social_website.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": config("DB_NAME"),
-#         "HOST": config("HOST"),
-#         "USER": config("USER"),
-#         "PORT": config("PORT", cast=int),
-#         "PASSWORD": config("PASSWORD"),
-#     }
-# }
-# Online DB
 DATABASES = {
-    "default": dj_database_url.parse(config("External_Database_URL")),
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": config("DB_NAME"),
+        "HOST": config("HOST"),
+        "USER": config("USER"),
+        "PORT": config("PORT", cast=int),
+        "PASSWORD": config("PASSWORD"),
+    }
 }
+# Online DB
+# DATABASES = {
+#     "default": dj_database_url.parse(config("External_Database_URL")),
+# }
 
 
 # Password validation
@@ -171,3 +171,16 @@ AUTHENTICATION_BACKENDS = [
 
 SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = config("GOOGLE_OAUTH2_KEY")
 SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = config("GOOGLE_OAUTH_SECRET")
+
+SOCIAL_AUTH_PIPELINE = [
+    "social_core.pipeline.social_auth.social_details",
+    "social_core.pipeline.social_auth.social_uid",
+    "social_core.pipeline.social_auth.auth_allowed",
+    "social_core.pipeline.social_auth.social_user",
+    "social_core.pipeline.user.get_username",
+    "social_core.pipeline.user.create_user",
+    "account.authentication.create_profile",
+    "social_core.pipeline.social_auth.associate_user",
+    "social_core.pipeline.social_auth.load_extra_data",
+    "social_core.pipeline.user.user_details",
+]
