706bffdb9305d194d34f50e16dd52a7ad7d4c267
alindafortunate <alindafortunate5@gmail.com>
2025-07-01 16:33:10 +0300

    Switched to the PG database

--- images/admin.py
+++ images/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
+from .models import Image
+
 
 # Register your models here.
+class ImageAdmin(ModelAdmin):
+    list_display = ["title", "slug", "image", "created"]
+    list_filter = ["created"]
+
+
+admin.site.register(Image, ImageAdmin)
