681b0e8f7956d6f2c8d5ac680b6bd7d64def1cdf
alindafortunate <alindafortunate5@gmail.com>
2025-07-07 22:13:21 +0300

    Understood the rsplit() method

--- images/forms.py
+++ images/forms.py
@@ -7,3 +7,13 @@ class ImageCreateForm(forms.ModelForm):
         model = Image
         fields = ["title", "url", "description"]
         widgets = {"url": forms.HiddenInput}
+
+    def clean_url(self):
+        url = self.cleaned_data["url"]
+        valid_extensions = ["jpg", "jpeg", "png"]
+        extension = url.rsplit(".", 1)[1].lower()
+        if extension not in valid_extensions:
+            raise (
+                "The given URL doesn't provided does not much valid image extension."
+            )
+        return url
