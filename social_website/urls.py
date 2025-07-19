85e038927cc6259d1ba7709115c3396941c8b648
alindafortunate <alindafortunate5@gmail.com>
2025-07-19 12:59:09 +0300

    Modified the main urls.py to include the urls for images.

--- social_website/urls.py
+++ social_website/urls.py
@@ -24,8 +24,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("account.urls")),
     path("social_auth/", include("social_django.urls", namespace="social")),
+    path("images/", include("images.urls", namespace="images")),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
- 
