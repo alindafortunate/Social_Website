9e95c09890cfcf705e135cf150aeff9f5e9076e3
alindafortunate <alindafortunate5@gmail.com>
2025-07-19 12:12:37 +0300

    Re-did the save() method overriding and understood it.

--- images/forms.py
+++ images/forms.py
@@ -1,3 +1,6 @@
+import requests
+from django.core.files.base import ContentFile
+from django.utils.text import slugify
 from django import forms
 from .models import Image
 
@@ -22,7 +25,17 @@ class ImageCreateForm(forms.ModelForm):
     # Today I didn't code as well.
     # force_insert=False, force_update=False,
     def save(self, force_insert=False, force_update=False, commit=True):
-        return super().save(commit)
+        image = super().save(commit=False)
+        image_url = self.cleaned_data["url"]
+        extension = image_url.rsplit(".", 1)[1].lower()
+        name = slugify(image.title)
+        image_name = f"{name}.{extension}"
+        # Dowload the image from the given URL.
+        response = requests.get(image_url)
+        image.image.save(image_name, ContentFile(response.content), save=False)
+        if commit:
+            image.save()
+        return image
 
     # Today I didn't code I was in field.
     # The return to divine instructions.
