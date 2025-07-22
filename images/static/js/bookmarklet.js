c92112a4b4d5ba6816d1df3ea09fcd507ca55e0c
alindafortunate <alindafortunate5@gmail.com>
2025-07-22 16:14:37 +0300

    Creating a div container where to select the images from.

--- images/static/js/bookmarklet.js
+++ images/static/js/bookmarklet.js
@@ -10,3 +10,13 @@ link.rel = 'stylesheet'
 link.type = 'text/css'
 link.href = styleUrl + '?r=' + Math.floor(Math.random() * 9999999999999999)
 head.appendChild(link)
+
+// load HTML
+const body = document.getElementsByTagName('body')[0]
+boxHtml = `
+    <div id='bookmarklet'>
+        <a href='#' id='close'>&times;</a>
+        <h1>Select an image to bookmark:</h1>
+        <div class = 'images'></div>
+    </div> `;
+body.innerHTML += boxHtml;
