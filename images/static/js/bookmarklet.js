4024c1f7db3457f9047097f16b90f79e9cc65f03
alindafortunate <alindafortunate5@gmail.com>
2025-07-28 11:57:48 +0300

    Understood where bookmarklet and .images are coming from on line 25 and 26 respectively.

--- images/static/js/bookmarklet.js
+++ images/static/js/bookmarklet.js
@@ -23,5 +23,15 @@ body.innerHTML += boxHtml;
 
 function bookmarkletLaunch() {
     bookmarklet = document.getElementById('bookmarklet');
+    const imagesFound = bookmarklet.queryselector('.images') // clear images found
+    imagesFound.innerHTML = '' //display bookmarklet
+    bookmarklet.style.display = 'block'
+    //close event
+    bookmarklet.queryselector('#close').addEventListener('click', function () {
+        bookmarklet.style.display = 'none'
+
+    });
 
 }
+//launch the bookmarklet
+bookmarkletLaunch();
