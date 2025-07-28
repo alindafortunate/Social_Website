6bb46d492f7c0648bc3e83fd7fe40dc5a6d490c0
alindafortunate <alindafortunate5@gmail.com>
2025-07-28 12:00:16 +0300

    The method is querySelector() not queryselector()

--- images/static/js/bookmarklet.js
+++ images/static/js/bookmarklet.js
@@ -23,11 +23,11 @@ body.innerHTML += boxHtml;
 
 function bookmarkletLaunch() {
     bookmarklet = document.getElementById('bookmarklet');
-    const imagesFound = bookmarklet.queryselector('.images') // clear images found
+    const imagesFound = bookmarklet.querySelector('.images') // clear images found
     imagesFound.innerHTML = '' //display bookmarklet
     bookmarklet.style.display = 'block'
     //close event
-    bookmarklet.queryselector('#close').addEventListener('click', function () {
+    bookmarklet.querySelector('#close').addEventListener('click', function () {
         bookmarklet.style.display = 'none'
 
     });
