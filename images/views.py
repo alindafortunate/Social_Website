2233fafdeef8980513edc4abb46c92126b377807
alindafortunate <alindafortunate5@gmail.com>
2025-08-27 11:52:39 +0300

    Understood that login_required decorator comes from django.contrib.auth.decorators where as require_POST comes from django.views.decorators.http

--- images/views.py
+++ images/views.py
@@ -1,6 +1,8 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect, render, get_object_or_404
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
 
 from .models import Image
 from .forms import ImageCreateForm
