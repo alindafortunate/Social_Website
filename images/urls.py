c32974617fc1e12395a4aea663803a669cc246d9
alindafortunate <alindafortunate5@gmail.com>
2025-08-27 12:27:51 +0300

    Added the url path for the image_like view.

--- images/urls.py
+++ images/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import create_image, image_detail
+from .views import create_image, image_detail, image_like
 
 app_name = "images"
 
 urlpatterns = [
     path("create/", create_image, name="create"),
     path("detail/<int:id>/<slug:slug>/", image_detail, name="detail"),
+    path("like/", image_like, name="like"),
 ]
