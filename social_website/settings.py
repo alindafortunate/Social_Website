80d7b73c1ef347253f26a237df4ea6ef7ea2152d
alindafortunate <alindafortunate5@gmail.com>
2025-09-02 16:00:15 +0300

    Implemented the ABSOLUTE_URL_OVERRIDES functionality but something is lacking.

--- social_website/settings.py
+++ social_website/settings.py
@@ -188,3 +188,10 @@ SOCIAL_AUTH_PIPELINE = [
     "social_core.pipeline.social_auth.load_extra_data",
     "social_core.pipeline.user.user_details",
 ]
+
+
+from django.urls import reverse_lazy
+
+ABSOLUTE_URL_OVERRIDES = {
+    "auth.user": lambda u: reverse_lazy("user_detail", args=[u.username])
+}
