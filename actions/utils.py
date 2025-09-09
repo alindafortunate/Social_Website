b7c72927ad5cd2a66e1d026d961e5f35f6f29ae2
alindafortunate <alindafortunate5@gmail.com>
2025-09-09 13:02:43 +0300

    Trying to avoid duplicated in users actions for the activity stream

--- actions/utils.py
+++ actions/utils.py
@@ -1,9 +1,19 @@
 # Creating a simple method to create action objects.
-
+import datetime
 from django.contrib.contenttypes.models import ContentType
+from django.utils import timezone
 from .models import Action
 
 
 def create_action(user, verb, target=None):
+    # Check the action that happened in the last one minute.
+    now = timezone.now()
+    last_minute = now - datetime.timedelta(seconds=60)
+    similar_actions = Action.objects.filter(
+        user=user.id, verb=verb, created__gte=last_minute
+    )
+    if target:
+        target_ct = ContentType.get_object_for_this_type(target)
+        similar_actions = Action.objects.filter(user_id=user.id, target_ct=target_ct, target_id=)
     action = Action(user=user, verb=verb, target=target)
     action.save()
