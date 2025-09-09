280d7d9f30e7451d416951331483fae24bad9281
alindafortunate <alindafortunate5@gmail.com>
2025-09-09 14:12:09 +0300

    Implemented the create_action functionality that eliminates the duplicates.

--- actions/utils.py
+++ actions/utils.py
@@ -10,10 +10,16 @@ def create_action(user, verb, target=None):
     now = timezone.now()
     last_minute = now - datetime.timedelta(seconds=60)
     similar_actions = Action.objects.filter(
-        user=user.id, verb=verb, created__gte=last_minute
+        user_id=user.id, verb=verb, created__gte=last_minute
     )
     if target:
-        target_ct = ContentType.get_object_for_this_type(target)
-        similar_actions = Action.objects.filter(user_id=user.id, target_ct=target_ct, target_id=)
-    action = Action(user=user, verb=verb, target=target)
-    action.save()
+        target_ct = ContentType.objects.get_for_model(target)
+        similar_actions = similar_actions.filter(
+            target_ct=target_ct, target_id=target.id
+        )
+    if not similar_actions:
+        # No existing actions found.
+        action = Action(user=user, verb=verb, target=target)
+        action.save()
+        return True
+    return False
