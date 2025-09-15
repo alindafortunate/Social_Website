c123a65ba48be906d9e39a9ccfd4e870652ca561
alindafortunate <alindafortunate5@gmail.com>
2025-09-15 16:55:41 +0300

    Implemented ranking using Redis

--- images/urls.py
+++ images/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import create_image, image_detail, image_like, image_list
+from .views import create_image, image_detail, image_like, image_list, image_ranking
 
 app_name = "images"
 
@@ -8,4 +8,5 @@ urlpatterns = [
     path("detail/<int:id>/<slug:slug>/", image_detail, name="detail"),
     path("like/", image_like, name="like"),
     path("", image_list, name="list"),
+    path("ranking/", image_ranking, name="ranking"),
 ]
--- images/views.py
+++ images/views.py
@@ -37,7 +37,10 @@ def create_image(request):
 
 def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
+    # Increment total image views by 1
     total_views = r.incr(f"image:{image.id}:views")
+    # Increment image ranking by 1
+    r.zincrby("image_ranking", 1, image.id)
     return render(
         request,
         "images/image/detail.html",
@@ -90,4 +93,19 @@ def image_list(request):
         )
     return render(
         request, "images/image/list.html", {"section": "images", "images": images}
+    )
+
+
+@login_required
+def image_ranking(request):
+    # get the image ranking dictionary
+    image_ranking = r.zrange("image_ranking", 0, -1, desc=True)[:10]
+    image_ranking_ids = [int(id) for id in image_ranking]
+    # get most viewed images
+    most_viewed = list(Image.objects.filter(id__in=image_ranking_ids))
+    most_viewed.sort(key=lambda x: image_ranking_ids.index(x.id))
+    return render(
+        request,
+        "images/image/ranking.html",
+        {"section": "images", "most_viewed": most_viewed},
     )
